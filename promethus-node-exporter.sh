c04259b9382e40cc56b809494461e2043b781ae0
cloudinstitution <134688786+cloudinstitution@users.noreply.github.com>
2025-11-22 23:37:11 +0530

    Create promethus-node-exporter.sh

---
+++ promethus-node-exporter.sh
@@ -0,0 +1,9 @@
+wget https://github.com/prometheus/node_exporter/releases/download/v1.1.2/node_exporter-1.1.2.linux-amd64.tar.gz
+tar -xvf node_exporter-1.1.2.linux-amd64.tar.gz
+mv node_exporter-1.1.2.linux-amd64/node_exporter /usr/local/bin/
+rm -rf node_exporter-1.1.2.linux-amd64/*
+sudo useradd -rs /bin/false node_exporter
+cp node_exporter.service /etc/systemd/system/node_exporter.service
+sudo systemctl enable node_exporter
+sudo systemctl start node_exporter
+sudo systemctl status node_exporter 
